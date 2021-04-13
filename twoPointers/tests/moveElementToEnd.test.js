84076000a8a22d0d7776b139c074490704d3bbf8
dfried514 <dfried514@gmail.com>
2021-04-13 13:02:06 -0700

    cleam up test of twoPointers

--- twoPointers/tests/moveElementToEnd.test.js
+++ twoPointers/tests/moveElementToEnd.test.js
@@ -8,9 +8,11 @@ const array2 = [5, 1, 2, 5, 5, 3, 4, 6, 7, 5, 8, 9, 10, 11, 5, 5, 12];
 const toMove2 = 5;
 const expected2 = [1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12, 5, 5, 5, 5, 5, 5];
 
-test('moves selected element to end of array', () => {
-  expect(moveElementToEnd(array1, toMove1)).toEqual(expected1);
-});
-test('moves selected element to end of array', () => {
-  expect(moveElementToEnd(array2, toMove2)).toEqual(expected2);
+describe('moveElementToEnd', () => {
+  test('moves selected element to end of array', () => {
+    expect(moveElementToEnd(array1, toMove1)).toEqual(expected1);
+  });
+  test('moves selected element to end of array', () => {
+    expect(moveElementToEnd(array2, toMove2)).toEqual(expected2);
+  });
 });
